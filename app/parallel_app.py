e2e4382e5d6cacdc4b8fe215eb23760aece2e20f
rathil <rathilvasani@gmail.com>
2020-11-17 22:26:05 +0530

    tmp update

--- app/parallel_app.py
+++ app/parallel_app.py
@@ -1,7 +1,5 @@
 import os
 #  Parallel Test Trigger
 
-os.system("pip install --upgrade pip")
-os.system("pip list")
-os.system("pip install Appium-python-client")
-os.system("python app/run_parallel_tests_app.py app/testrun_app.py app/device.json")
+
+os.system("python3 app/run_parallel_tests_app.py app/testrun_app.py app/device.json")
